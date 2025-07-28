3380ebf737bbf85ef986bdcd3e4557e9aaa6b361
Catalin Cirstoiu <catalin.cirstoiu@gmail.com>
2025-07-28 22:41:56 +0200

    force https proxy from .env

--- app/settings.py
+++ app/settings.py
@@ -6,4 +6,4 @@ dotenv.load_dotenv()
 repo_dir = os.environ["REPO_DIR"]
 pa_api_token = os.environ["PA_API_TOKEN"]
 twelvedata_api_key = os.environ["TWELVEDATA_API_KEY"]
-https_proxy = os.environ.get("https_proxy", "")
+https_proxy = os.environ.get("HTTPS_PROXY")
